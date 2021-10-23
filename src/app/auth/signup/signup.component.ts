80b0200cd547698770b9b0e13d0b1f0f1e926e7d
Adam Pawelczyk <pawelczyk.adam@hotmail.com>
2021-10-23 22:46:24 +0200

    Add toastr notifications for signup

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import {FormControl, FormGroup, Validators} from "@angular/forms";
 import {SignupRequestPayload} from "./signup-request.payload";
 import {AuthService} from "../shared/auth.service";
+import {Router} from "@angular/router";
+import {ToastrService} from "ngx-toastr";
 
 @Component({
   selector: 'app-signup',
@@ -12,7 +14,7 @@ export class SignupComponent implements OnInit {
   signupForm: FormGroup;
   signupRequestPayload: SignupRequestPayload;
 
-  constructor(private authService: AuthService) {
+  constructor(private authService: AuthService, private router: Router, private toastr: ToastrService) {
     this.signupRequestPayload = {
       username: '',
       email: '',
@@ -34,7 +36,9 @@ export class SignupComponent implements OnInit {
     this.signupRequestPayload.password = this.signupForm.get('password')?.value;
 
     this.authService.signup(this.signupRequestPayload).subscribe(data => {
-      console.log(data);
+      this.router.navigate(['/login'], {queryParams: {registered: 'true'}});
+    }, () => {
+      this.toastr.error('Registration failed! Please try again');
     });
   }
 
