67803d01cd06d441d038ff5d1e565f17f018b11e
Adam Pawelczyk <pawelczyk.adam@hotmail.com>
2021-10-25 22:32:03 +0200

    Fix TokenInterceptor

--- src/app/token-interceptor.ts
+++ src/app/token-interceptor.ts
@@ -1,9 +1,9 @@
-import {Injectable} from "@angular/core";
-import {HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from "@angular/common/http";
-import {AuthService} from "./auth/shared/auth.service";
-import {BehaviorSubject, Observable, throwError} from "rxjs";
-import {catchError, switchMap} from "rxjs/operators";
-import {LoginResponsePayload} from "./auth/login/login-response.payload";
+import { Injectable } from '@angular/core';
+import { HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
+import { AuthService } from "./auth/shared/auth.service";
+import { BehaviorSubject, Observable, throwError } from "rxjs";
+import { catchError, switchMap, take, filter } from 'rxjs/operators';
+import { LoginResponsePayload } from './auth/login/login-response.payload';
 
 @Injectable({
   providedIn: 'root'
@@ -15,39 +15,44 @@ export class TokenInterceptor implements HttpInterceptor {
   constructor(public authService: AuthService) {}
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    if (req.url.indexOf('refresh') !== -1 || req.url.indexOf('login') !== -1) {
+      return next.handle(req);
+    }
+
     const jwtToken = this.authService.getJwtToken();
 
     if (jwtToken) {
-      this.addToken(req, jwtToken);
+      return next.handle(this.addToken(req, jwtToken)).pipe(catchError(error => {
+        if (error instanceof HttpErrorResponse && error.status === 403) {
+          return this.handleAuthErrors(req, next);
+        } else {
+          return throwError(error);
+        }
+      }));
     }
-
-    return next.handle(req).pipe(catchError(error => {
-      if (error instanceof HttpErrorResponse && error.status === 403) {
-        return this.handleAuthErrors(req, next);
-      } else {
-        return throwError(error);
-      }
-    }))
+    return next.handle(req);
   }
 
-  // @ts-ignore
   private handleAuthErrors(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     if (!this.isTokenRefreshing) {
       this.isTokenRefreshing = true;
       this.refreshTokenSubject.next(null);
 
       return this.authService.refreshToken().pipe(switchMap((refreshTokenResponse: LoginResponsePayload) => {
-        this.isTokenRefreshing = false;
-        this.refreshTokenSubject.next(refreshTokenResponse.authenticationToken);
-
-        return next.handle(this.addToken(req, refreshTokenResponse.authenticationToken));
-      }))
+          this.isTokenRefreshing = false;
+          this.refreshTokenSubject.next(refreshTokenResponse.authenticationToken);
+          return next.handle(this.addToken(req, refreshTokenResponse.authenticationToken));
+        }))
+    } else {
+      return this.refreshTokenSubject.pipe(filter(result => result !== null), take(1), switchMap(() => {
+          return next.handle(this.addToken(req, this.authService.getJwtToken()))
+        }))
     }
   }
 
   addToken(req: HttpRequest<any>, jwtToken: any) {
     return req.clone({
-      headers: req.headers.set('Authorization', 'Bearer' + jwtToken)
+      headers: req.headers.set('Authorization', 'Bearer ' + jwtToken)
     })
   }
 }
