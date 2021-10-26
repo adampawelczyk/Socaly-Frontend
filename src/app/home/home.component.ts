e7b093cc384127e0323897df9fd61c8406380553
Adam Pawelczyk <pawelczyk.adam@hotmail.com>
2021-10-26 21:43:36 +0200

    Move constructor and variables to post-tile component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import {PostModel} from "../shared/post-model";
-import {PostService} from "../shared/post.service";
-import {faArrowUp, faArrowDown, faComments} from "@fortawesome/free-solid-svg-icons";
 
 @Component({
   selector: 'app-home',
@@ -9,17 +6,7 @@ import {faArrowUp, faArrowDown, faComments} from "@fortawesome/free-solid-svg-ic
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  faArrowUp = faArrowUp;
-  faArrowDown = faArrowDown;
-  faComments = faComments;
-
-  posts$: Array<PostModel> = [];
-
-  constructor(private postService: PostService) {
-    this.postService.getAllPosts().subscribe(post => {
-      this.posts$ = post;
-    })
-  }
+  constructor() {}
 
   ngOnInit(): void {
   }
