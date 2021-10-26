c8164c4df9290b6ccc699105f8acc030272f6708
Adam Pawelczyk <pawelczyk.adam@hotmail.com>
2021-10-26 21:28:49 +0200

    Create community-side-bar component

---
+++ src/app/shared/community-side-bar/community-side-bar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-community-side-bar',
+  templateUrl: './community-side-bar.component.html',
+  styleUrls: ['./community-side-bar.component.css']
+})
+export class CommunitySideBarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
