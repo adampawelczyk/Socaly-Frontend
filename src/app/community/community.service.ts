a571e3e0d01a548f900033892d829b025a15cdd2
Adam Pawelczyk <pawelczyk.adam@hotmail.com>
2021-10-27 21:45:19 +0200

    Create community service

---
+++ src/app/community/community.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {CommunityResponse} from "./community-response";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CommunityService {
+
+  constructor(private http: HttpClient) { }
+
+  getAllCommunities(): Observable<Array<CommunityResponse>> {
+    return this.http.get<Array<CommunityResponse>>('http://localhost:8090/api/community')
+  }
+}
