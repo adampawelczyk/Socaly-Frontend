030c70e8d49727a4de2748d91bef9ec641f6eead
Adam Pawelczyk <pawelczyk.adam@hotmail.com>
2021-10-30 21:02:29 +0200

    Create variables and initialize them in the constructor, create discard and createCommunity methods

--- src/app/community/create-community/create-community.component.ts
+++ src/app/community/create-community/create-community.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {Router} from "@angular/router";
+import {CommunityService} from "../community.service";
+import {CommunityResponse} from "../community-response";
 
 @Component({
   selector: 'app-create-community',
@@ -6,10 +10,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./create-community.component.css']
 })
 export class CreateCommunityComponent implements OnInit {
+  createCommunityForm: FormGroup;
+  communityResponse: CommunityResponse;
+  title = new FormControl('');
+  description = new FormControl('');
 
-  constructor() { }
+  constructor(private router: Router, private communityService: CommunityService) {
+    this.createCommunityForm = new FormGroup({
+      title: new FormControl('', Validators.required),
+      description: new FormControl('', Validators.required)
+    });
+    this.communityResponse = {
+      name: '',
+      description: ''
+    }
+  }
 
   ngOnInit(): void {
+  }
+
+  discard() {
+    this.router.navigateByUrl('/');
+  }
+
+  createCommunity() {
+    this.communityResponse.name = this.createCommunityForm.get('title')?.value;
+    this.communityResponse.description = this.createCommunityForm.get('description')?.value;
+
+    this.communityService.createCommunity(this.communityResponse).subscribe(data => {
+      this.router.navigateByUrl('/communities');
+    }, error => {
+      console.log('Error occurred');
+    })
   }
 
 }
