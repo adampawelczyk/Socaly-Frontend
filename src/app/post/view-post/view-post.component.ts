baadac791986be809a01b737e7ab3f07acba7d6b
Adam Pawelczyk <pawelczyk.adam@hotmail.com>
2021-11-03 23:57:10 +0100

    Add new variables, change constructor, create postComment, getPostById and getCommentsForPost methods

--- src/app/post/view-post/view-post.component.ts
+++ src/app/post/view-post/view-post.component.ts
@@ -3,6 +3,9 @@ import {PostModel} from "../../shared/post-model";
 import {PostService} from "../../shared/post.service";
 import {ActivatedRoute} from "@angular/router";
 import {throwError} from "rxjs";
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {CommentPayload} from "../../comment/comment.payload";
+import {CommentService} from "../../comment/comment.service";
 
 @Component({
   selector: 'app-view-post',
@@ -12,9 +15,41 @@ import {throwError} from "rxjs";
 export class ViewPostComponent implements OnInit {
   postId: number;
   post: PostModel;
+  commentForm: FormGroup;
+  commentPayload: CommentPayload;
+  comments: CommentPayload[];
 
-  constructor(private postService: PostService, private activateRoute: ActivatedRoute) {
+  constructor(private postService: PostService, private activateRoute: ActivatedRoute,
+              private commentService: CommentService) {
     this.postId = this.activateRoute.snapshot.params.id;
+
+    this.commentForm = new FormGroup({
+      text: new FormControl('', Validators.required)
+    });
+
+    this.commentPayload = {
+      text: '',
+      postId: this.postId
+    };
+  }
+
+  ngOnInit(): void {
+    this.getPostById();
+    this.getCommentsForPost();
+  }
+
+  postComment() {
+    this.commentPayload.text = this.commentForm.get('text')?.value;
+
+    this.commentService.postComment(this.commentPayload).subscribe(data => {
+      this.commentForm.get('text')?.setValue('');
+      this.getCommentsForPost();
+    }, error => {
+      throwError(error);
+    });
+  }
+
+  private getPostById() {
     this.postService.getPost(this.postId).subscribe(data => {
       this.post = data;
     }, error => {
@@ -22,7 +57,11 @@ export class ViewPostComponent implements OnInit {
     });
   }
 
-  ngOnInit(): void {
+  private getCommentsForPost() {
+    this.commentService.getAllCommentsForPost(this.postId).subscribe(data => {
+      this.comments = data;
+    }, error => {
+      throwError(error);
+    });
   }
-
 }
