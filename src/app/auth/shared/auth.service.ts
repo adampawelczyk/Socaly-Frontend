64f404e731083d3c90faeebb9b174efeb13f64c9
Adam Pawelczyk <pawelczyk.adam@hotmail.com>
2021-11-11 22:01:19 +0100

    create event emitter variables and refreshTokenPayload variable

--- src/app/auth/shared/auth.service.ts
+++ src/app/auth/shared/auth.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import {EventEmitter, Injectable, Output} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {SignupRequestPayload} from "../signup/signup-request.payload";
 import {Observable} from "rxjs";
@@ -11,6 +11,13 @@ import {LocalStorageService} from "ngx-webstorage";
   providedIn: 'root'
 })
 export class AuthService {
+  @Output() loggedIn: EventEmitter<boolean> = new EventEmitter();
+  @Output() username: EventEmitter<string> = new EventEmitter();
+
+  refreshTokenPayload = {
+    refreshToken: this.getRefreshToken(),
+    username: this.getUsername()
+  }
 
   constructor(private httpClient: HttpClient, private localStorage: LocalStorageService) { }
 
