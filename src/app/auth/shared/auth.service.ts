11fbf474403e368b199530ff05ce7e65d5f94a91
Adam Pawelczyk <pawelczyk.adam@hotmail.com>
2021-11-11 22:03:37 +0100

    clear localStorage

--- src/app/auth/shared/auth.service.ts
+++ src/app/auth/shared/auth.service.ts
@@ -47,6 +47,9 @@ export class AuthService {
   refreshToken() {
     return this.httpClient.post<LoginResponsePayload>('http://localhost:8090/api/auth/refresh/token', this.refreshTokenPayload)
       .pipe(tap(response => {
+        this.localStorage.clear('authenticationToken');
+        this.localStorage.clear('expiresAt');
+
         this.localStorage.store('authenticationToken', response.authenticationToken);
         this.localStorage.store('expiresAt', response.expiresAt);
       }))
