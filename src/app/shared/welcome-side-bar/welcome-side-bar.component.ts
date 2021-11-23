882ebe2b6047d8554232100c9c84c1d552b71285
Adam Pawelczyk <pawelczyk.adam@hotmail.com>
2021-11-23 23:46:41 +0100

    create welcome-side-bar

---
+++ src/app/shared/welcome-side-bar/welcome-side-bar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-welcome-side-bar',
+  templateUrl: './welcome-side-bar.component.html',
+  styleUrls: ['./welcome-side-bar.component.css']
+})
+export class WelcomeSideBarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
