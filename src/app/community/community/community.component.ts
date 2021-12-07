842576e942d2e35e83707a70c844a5672c2eca76
Adam Pawelczyk <pawelczyk.adam@hotmail.com>
2021-12-07 23:59:38 +0100

    add variables and initialize them

--- src/app/community/community/community.component.ts
+++ src/app/community/community/community.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {PostModel} from "../../shared/post-model";
+import {PostService} from "../../shared/post.service";
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-community',
@@ -6,8 +9,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./community.component.css']
 })
 export class CommunityComponent implements OnInit {
+  communityId: number;
+  posts: PostModel[];
+  postLength: number;
 
-  constructor() { }
+  constructor(private activatedRoute: ActivatedRoute, private postService: PostService) {
+    this.communityId = this.activatedRoute.snapshot.params.id;
+
+    this.postService.getAllPostsByCommunity(this.communityId).subscribe(data => {
+      this.posts = data;
+      this.postLength = data.length;
+    })
+  }
 
   ngOnInit(): void {
   }
