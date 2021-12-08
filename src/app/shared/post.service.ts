b2a5a9bcf3fd3cc0c690d9b0182dbf9475d3f870
Adam Pawelczyk <pawelczyk.adam@hotmail.com>
2021-12-08 13:09:33 +0100

    change getting all community posts by name instead of id

--- src/app/shared/post.service.ts
+++ src/app/shared/post.service.ts
@@ -27,7 +27,7 @@ export class PostService {
     return this.http.get<PostModel[]>('http://localhost:8090/api/posts/by-user/' + name)
   }
 
-  getAllPostsByCommunity(id: number): Observable<PostModel[]> {
-    return this.http.get<PostModel[]>('http://localhost:8090/api/posts/by-community/' + id)
+  getAllPostsByCommunity(name: String): Observable<PostModel[]> {
+    return this.http.get<PostModel[]>('http://localhost:8090/api/posts/by-community/' + name)
   }
 }
