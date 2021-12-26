3b3063db559925c5e70d98436c67d80520941158
Adam Pawelczyk <pawelczyk.adam@hotmail.com>
2021-12-26 16:16:45 +0100

    add NgbModal to the constructor and add function to signup

--- src/app/shared/welcome-side-bar/welcome-side-bar.component.ts
+++ src/app/shared/welcome-side-bar/welcome-side-bar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {NgbModal} from "@ng-bootstrap/ng-bootstrap";
+import {SignupComponent} from "../../auth/signup/signup.component";
 
 @Component({
   selector: 'app-welcome-side-bar',
@@ -7,9 +9,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WelcomeSideBarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private modal: NgbModal) { }
 
   ngOnInit(): void {
   }
 
+  signup() {
+    this.modal.open(SignupComponent);
+  }
 }
