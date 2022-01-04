0d2ba3c52a376a6df24ea4fa67cc8c7732f83be0
Adam Pawelczyk <pawelczyk.adam@hotmail.com>
2022-01-04 23:54:46 +0100

    add imports, add variables and add editorconfig

--- src/app/shared/post-tile/post-tile.component.ts
+++ src/app/shared/post-tile/post-tile.component.ts
@@ -2,7 +2,11 @@ import {Component, Input, OnInit} from '@angular/core';
 import {PostService} from "../post.service";
 import {PostModel} from "../post-model";
 import {faCommentAlt} from "@fortawesome/free-solid-svg-icons";
-import {Router} from "@angular/router";
+import {ActivatedRoute, Router} from "@angular/router";
+import {throwError} from "rxjs";
+import {CommentService} from "../../comment/comment.service";
+import {CommentPayload} from "../../comment/comment.payload";
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-post-tile',
@@ -17,6 +21,37 @@ export class PostTileComponent implements OnInit {
   faCommentAlt = faCommentAlt;
   @Input()
   posts$: PostModel[];
+  comments: CommentPayload[];
+  dataModel: string
+
+  postId: number;
+  commentForm: FormGroup;
+  commentPayload: CommentPayload;
+
+  editorConfig = {
+    skin_url: '..\\assets\\skins\\ui\\light',
+    icons: 'material',
+    branding: false,
+    height: 174,
+    placeholder: "What are your thoughts?",
+    menubar: false,
+    plugins: [
+      'advlist lists charmap print preview anchor emoticons',
+      'searchreplace visualblocks code fullscreen',
+      'insertdatetime media table paste codesample link toc'
+    ],
+    toolbar:
+      'formatselect | bold italic link strikethrough superscript bullist numlist emoticons',
+    link_title: false,
+    target_list: false,
+    default_link_target:"_blank",
+    link_context_toolbar: true,
+    codesample_content_css: '..\\assets\\prism.css',
+  }
+
+  constructor(private postService: PostService, private router: Router, private commentService: CommentService,
+              private activateRoute: ActivatedRoute) {
+    this.postId = this.activateRoute.snapshot.params.id;
 
   constructor(private postService: PostService, private router: Router) { }
 
