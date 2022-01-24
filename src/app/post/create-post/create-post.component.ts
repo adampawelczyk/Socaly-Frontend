3010afc573ff5d7ed7ab3c13bc01781d32398a8c
Adam Pawelczyk <pawelczyk.adam@hotmail.com>
2022-01-24 23:48:16 +0100

    add variables

--- src/app/post/create-post/create-post.component.ts
+++ src/app/post/create-post/create-post.component.ts
@@ -17,6 +17,10 @@ export class CreatePostComponent implements OnInit {
   createPostForm: FormGroup
   postPayload: CreatePostPayload
   communities: Array<CommunityResponse>
+  files: File[] = []
+  fileUrls: string[] = []
+  active = 1
+  selectedCommunity = "Choose a community"
 
   editorConfig = {
     skin_url: 'assets\\skins\\ui\\light',
