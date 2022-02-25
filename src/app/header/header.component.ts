3ba5134ab2761cfd3a0a9a1e1996e1f38306ba02
Adam Pawelczyk <pawelczyk.adam@hotmail.com>
2022-02-26 00:30:38 +0100

    change import route

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {AuthService} from "../auth/shared/auth.service";
+import {AuthService} from "../auth/auth.service";
 import {Router} from "@angular/router";
 import {NgbModal} from "@ng-bootstrap/ng-bootstrap";
 import {SignupComponent} from "../auth/signup/signup.component";
