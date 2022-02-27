a90c9c063e468ed8afa28d0ff8424a3ac80cde2b
Adam Pawelczyk <pawelczyk.adam@hotmail.com>
2022-02-28 00:17:22 +0100

    Change import routes

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {FormControl, FormGroup, Validators} from "@angular/forms";
 import {SignupRequestPayload} from "./signup-request.payload";
-import {AuthService} from "../auth.service";
+import {AuthService} from "../shared/auth.service";
 import {Router} from "@angular/router";
 import {ToastrService} from "ngx-toastr";
 import {NgbActiveModal, NgbModal} from "@ng-bootstrap/ng-bootstrap";
