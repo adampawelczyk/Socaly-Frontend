f40335484a4aa38b43a4e02e13ee229198651b5c
Adam Pawelczyk <pawelczyk-adam@outlook.com>
2022-03-09 23:50:36 +0100

    Create file service to upload and remove files

---
+++ src/app/shared/file.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core'
+import { AngularFireDatabase } from "@angular/fire/compat/database"
+import { AngularFireStorage } from "@angular/fire/compat/storage"
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FileService {
+  private basePath = '/uploads'
+
+  constructor(private db: AngularFireDatabase, private storage: AngularFireStorage) { }
+
+  async uploadFile(file: File) {
+    const filePath = `${this.basePath}/${file.name}`
+    const snap = await this.storage.upload(filePath, file)
+
+    return await snap.ref.getDownloadURL()
+  }
+
+  removeFile(url: string) {
+    this.storage.refFromURL(url).delete()
+  }
+}
