acb9b1b3c6ed5090393e277f87ab5c6be2eb14a3
Adam Pawelczyk <pawelczyk-adam@outlook.com>
2022-03-12 21:14:54 +0100

    Create home-sidebar component

---
+++ src/app/panes/home-sidebar/home-sidebar.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core'
+import { CreateCommunityComponent } from "../../community/create-community/create-community.component"
+import { NgbModal } from "@ng-bootstrap/ng-bootstrap"
+import {CreatePostComponent} from "../../post/create-post/create-post.component"
+
+@Component({
+  selector: 'app-home-sidebar',
+  templateUrl: './home-sidebar.component.html',
+  styleUrls: ['./home-sidebar.component.css']
+})
+export class HomeSidebarComponent implements OnInit {
+
+  constructor(private modal: NgbModal) { }
+
+  ngOnInit(): void {
+  }
+
+  createPost() {
+    this.modal.open(CreatePostComponent, {size: 'lg'})
+  }
+
+  createCommunity() {
+    this.modal.open(CreateCommunityComponent)
+  }
+}
