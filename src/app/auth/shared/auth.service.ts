b3e8c0aa64a73d8ef4c045eaf7869c77c99c5eee
Adam Pawelczyk <pawelczyk-adam@outlook.com>
2022-03-20 23:34:52 +0100

    Add spacing

--- src/app/auth/shared/auth.service.ts
+++ src/app/auth/shared/auth.service.ts
@@ -1,11 +1,11 @@
-import {EventEmitter, Injectable, Output} from '@angular/core';
-import {HttpClient} from "@angular/common/http";
-import {SignupModel} from "../signup/signup.model";
-import {Observable, throwError} from "rxjs";
-import {LoginRequestModel} from "../login/login-request.model";
-import {LoginResponseModel} from "../login/login-response.model";
-import {map, tap} from "rxjs/operators";
-import {LocalStorageService} from "ngx-webstorage";
+import { EventEmitter, Injectable, Output } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { SignupModel } from "../signup/signup.model";
+import { Observable, throwError } from "rxjs";
+import { LoginRequestModel } from "../login/login-request.model";
+import { LoginResponseModel } from "../login/login-response.model";
+import { map, tap } from "rxjs/operators";
+import { LocalStorageService } from "ngx-webstorage";
 
 @Injectable({
   providedIn: 'root'
