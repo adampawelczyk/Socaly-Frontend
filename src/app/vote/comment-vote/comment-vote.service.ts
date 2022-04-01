d72158c0780f3a7b1c64e9e56121231978c4f2c5
Adam Pawelczyk <pawelczyk-adam@outlook.com>
2022-04-01 23:03:42 +0200

    Add spacing

--- src/app/vote/comment-vote/comment-vote.service.ts
+++ src/app/vote/comment-vote/comment-vote.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core'
 import { HttpClient } from "@angular/common/http"
 import { CommentVoteModel } from "./comment-vote.model"
-import {Observable} from "rxjs"
+import { Observable } from "rxjs"
 
 @Injectable({
   providedIn: 'root'
