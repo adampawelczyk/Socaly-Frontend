3b50ea6837dd72531c37646333942b4da3fc3522
Adam Pawelczyk <pawelczyk-adam@outlook.com>
2022-04-02 21:31:53 +0200

    Rename VoteModel to PostVoteModel and replace VoteModel with  CommentVoteModel in voteOnComment method

--- src/app/vote/shared/vote.service.ts
+++ src/app/vote/shared/vote.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
-import { VoteModel } from "./vote.model";
+import { PostVoteModel } from "./post-vote.model";
+import { CommentVoteModel } from "./comment-vote.model";
 import { Observable } from "rxjs";
 
 @Injectable({
@@ -10,11 +11,11 @@ export class VoteService {
 
   constructor(private httpClient: HttpClient) { }
 
-  voteOnPost(voteModel: VoteModel): Observable<any> {
+  voteOnPost(voteModel: PostVoteModel): Observable<any> {
     return this.httpClient.post('http://localhost:8090/api/votes/', voteModel);
   }
 
-  voteOnComment(voteModel: VoteModel): Observable<any> {
-    return this.httpClient.post('http://localhost:8090/api/comments/vote', voteModel)
+  voteOnComment(commentVoteModel: CommentVoteModel): Observable<any> {
+    return this.httpClient.post('http://localhost:8090/api/comments/vote', commentVoteModel)
   }
 }
