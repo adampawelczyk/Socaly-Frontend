5f9c497baf4fc09e1b65b412aff5a105218870d8
Adam Pawelczyk <pawelczyk-adam@outlook.com>
2022-04-02 17:34:00 +0200

    Move to shared folder

--- src/app/vote/shared/vote.service.ts
+++ src/app/vote/shared/vote.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
-import { VoteModel } from "../shared/vote.model";
+import { VoteModel } from "./vote.model";
 import { Observable } from "rxjs";
 
 @Injectable({
