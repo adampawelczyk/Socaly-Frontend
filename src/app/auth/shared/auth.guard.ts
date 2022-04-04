6def6d9ed5ae97bbc1471dfe31f32485f0749995
Adam Pawelczyk <pawelczyk-adam@outlook.com>
2022-04-04 23:28:08 +0200

    Code refactoring

--- src/app/auth/shared/auth.guard.ts
+++ src/app/auth/shared/auth.guard.ts
@@ -1,7 +1,7 @@
-import { Injectable } from '@angular/core'
-import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router'
-import { Observable } from 'rxjs'
-import { AuthService } from "./auth.service"
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthService } from "./auth.service";
 
 @Injectable({
   providedIn: 'root'
@@ -13,14 +13,14 @@ export class AuthGuard implements CanActivate {
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
 
-    const isAuthenticated = this.authService.isLoggedIn()
+    const isAuthenticated = this.authService.isLoggedIn();
 
     if (isAuthenticated) {
-      return true
+      return true;
     } else {
-      this.router.navigateByUrl('/login')
+      this.router.navigateByUrl('/login');
     }
 
-    return false
-  }
+    return false;
+  };
 }
