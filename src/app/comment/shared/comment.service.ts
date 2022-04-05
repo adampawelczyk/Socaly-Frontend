deccb457d74755a69b812dc8350c110e503b1357
Adam Pawelczyk <pawelczyk-adam@outlook.com>
2022-04-05 22:43:05 +0200

    Code refactoring

--- src/app/comment/shared/comment.service.ts
+++ src/app/comment/shared/comment.service.ts
@@ -1,33 +1,32 @@
-import { Injectable } from '@angular/core'
-import { HttpClient } from "@angular/common/http"
-import { Observable } from "rxjs"
-import { CommentRequestModel } from "./comment-request.model"
-import { CommentResponseModel } from "./comment-response.model"
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { CommentRequestModel } from './comment-request.model';
+import { CommentResponseModel } from './comment-response.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CommentService {
-
   constructor(private httpClient: HttpClient) { }
 
   getComment(commentId: number): Observable<CommentResponseModel> {
-    return this.httpClient.get<CommentResponseModel>('http://localhost:8090/api/comments/' + commentId)
+    return this.httpClient.get<CommentResponseModel>('http://localhost:8090/api/comments/' + commentId);
   }
 
   getAllCommentsForPost(postId: number): Observable<CommentResponseModel[]> {
-    return this.httpClient.get<CommentResponseModel[]>('http://localhost:8090/api/comments/by-post/' + postId)
+    return this.httpClient.get<CommentResponseModel[]>('http://localhost:8090/api/comments/by-post/' + postId);
   }
 
   getSubCommentsForComment(commentId: number): Observable<CommentResponseModel[]> {
-    return this.httpClient.get<CommentResponseModel[]>('http://localhost:8090/api/comments/subcomments/' + commentId)
+    return this.httpClient.get<CommentResponseModel[]>('http://localhost:8090/api/comments/subcomments/' + commentId);
   }
 
   postComment(commentPayload: CommentRequestModel): Observable<any> {
-    return this.httpClient.post<any>('http://localhost:8090/api/comments', commentPayload)
+    return this.httpClient.post<any>('http://localhost:8090/api/comments', commentPayload);
   }
 
-  getAllCommentsByUser(name: String) {
-    return this.httpClient.get<CommentResponseModel[]>('http://localhost:8090/api/comments/by-user/' + name)
+  getAllCommentsByUser(name: string) {
+    return this.httpClient.get<CommentResponseModel[]>('http://localhost:8090/api/comments/by-user/' + name);
   }
 }
