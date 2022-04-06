0c4910241cc629b3052a12e4889a8c31f2b57dab
Adam Pawelczyk <pawelczyk-adam@outlook.com>
2022-04-06 23:49:52 +0200

    Code refactoring

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,9 +1,9 @@
-import { Component, OnInit } from '@angular/core'
-import { AuthService } from "../auth/shared/auth.service"
-import { Router } from "@angular/router"
-import { NgbModal } from "@ng-bootstrap/ng-bootstrap"
-import { SignupComponent } from "../auth/signup/signup.component"
-import { LoginComponent } from "../auth/login/login.component"
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../auth/shared/auth.service';
+import { Router } from '@angular/router';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { SignupComponent } from '../auth/signup/signup.component';
+import { LoginComponent } from '../auth/login/login.component';
 
 @Component({
   selector: 'app-header',
@@ -11,33 +11,33 @@ import { LoginComponent } from "../auth/login/login.component"
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-  isLoggedIn: boolean
-  username: string
+  isLoggedIn: boolean;
+  username: string;
 
   constructor(private authService: AuthService, private router: Router, private modal: NgbModal) { }
 
   ngOnInit(): void {
-    this.authService.loggedIn.subscribe((data: boolean) => this.isLoggedIn = data)
-    this.authService.username.subscribe((data: string) => this.username = data)
-    this.isLoggedIn = this.authService.isLoggedIn()
-    this.username = this.authService.getUsername()
+    this.authService.loggedIn.subscribe((data: boolean) => this.isLoggedIn = data);
+    this.authService.username.subscribe((data: string) => this.username = data);
+    this.isLoggedIn = this.authService.isLoggedIn();
+    this.username = this.authService.getUsername();
   }
 
   signup() {
-    this.modal.open(SignupComponent)
+    this.modal.open(SignupComponent);
   }
 
   login() {
-    this.modal.open(LoginComponent)
+    this.modal.open(LoginComponent);
   }
 
   goToUserProfile() {
-    this.router.navigateByUrl('/user-profile/' + this.username)
+    this.router.navigateByUrl('/user-profile/' + this.username);
   }
 
   logout() {
-    this.authService.logout()
-    this.isLoggedIn = false
-    this.router.navigateByUrl('')
+    this.authService.logout();
+    this.isLoggedIn = false;
+    this.router.navigateByUrl('');
   }
 }
