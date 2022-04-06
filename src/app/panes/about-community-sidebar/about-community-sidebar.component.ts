b87e6ec36a2cd102b17556a910c5ba6991d233b0
Adam Pawelczyk <pawelczyk-adam@outlook.com>
2022-04-06 23:50:35 +0200

    Code refactoring

--- src/app/panes/about-community-sidebar/about-community-sidebar.component.ts
+++ src/app/panes/about-community-sidebar/about-community-sidebar.component.ts
@@ -1,6 +1,6 @@
-import { Component, Input, OnInit } from '@angular/core'
-import { faBirthdayCake } from "@fortawesome/free-solid-svg-icons"
-import { CommunityService } from "../../community/shared/community.service"
+import { Component, Input, OnInit } from '@angular/core';
+import { faBirthdayCake } from '@fortawesome/free-solid-svg-icons';
+import { CommunityService } from '../../community/shared/community.service';
 
 @Component({
   selector: 'app-about-community-sidebar',
@@ -8,22 +8,21 @@ import { CommunityService } from "../../community/shared/community.service"
   styleUrls: ['./about-community-sidebar.component.css']
 })
 export class AboutCommunitySidebarComponent implements OnInit {
-  @Input() communityName: string
-
-  communityDescription: string
-  createdDate: string | undefined
-  faBirthdayCake = faBirthdayCake
+  @Input() communityName: string;
+  communityDescription: string;
+  createdDate: string | undefined;
+  faBirthdayCake = faBirthdayCake;
 
   constructor(private communityService: CommunityService) {}
 
   ngOnInit(): void {
     this.communityService.getCommunityDetails(this.communityName).subscribe(data => {
-      this.communityDescription = data.description
-      this.createdDate = this.convertTimeToMs(data.createdDate!)
-    })
+      this.communityDescription = data.description;
+      this.createdDate = this.convertTimeToMs(data.createdDate!);
+    });
   }
 
   convertTimeToMs(time: string): string {
-    return (parseInt(time) * 1000).toString()
+    return (parseInt(time) * 1000).toString();
   }
 }
