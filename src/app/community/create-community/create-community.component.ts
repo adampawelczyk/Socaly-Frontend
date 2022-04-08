a3aaaa4706959ba2cae6a752e510e1f49b02732e
Adam Pawelczyk <pawelczyk-adam@outlook.com>
2022-04-08 23:56:12 +0200

    Change CommunityModel to CommunityRequestModel

--- src/app/community/create-community/create-community.component.ts
+++ src/app/community/create-community/create-community.component.ts
@@ -2,8 +2,8 @@ import { Component, OnInit } from '@angular/core'
 import { FormControl, FormGroup, Validators } from "@angular/forms"
 import { Router } from "@angular/router"
 import { CommunityService } from "../shared/community.service"
-import { CommunityModel } from "../shared/community.model"
 import { NgbActiveModal } from "@ng-bootstrap/ng-bootstrap"
+import {CommunityRequestModel} from "../shared/community-request.model";
 
 @Component({
   selector: 'app-create-community',
@@ -12,7 +12,7 @@ import { NgbActiveModal } from "@ng-bootstrap/ng-bootstrap"
 })
 export class CreateCommunityComponent implements OnInit {
   createCommunityForm: FormGroup
-  communityResponse: CommunityModel
+  communityRequestModel: CommunityRequestModel
   title = new FormControl('')
   description = new FormControl('')
 
@@ -21,7 +21,7 @@ export class CreateCommunityComponent implements OnInit {
       title: new FormControl('', Validators.required),
       description: new FormControl('', Validators.required)
     })
-    this.communityResponse = {
+    this.communityRequestModel = {
       name: '',
       description: ''
     }
@@ -31,10 +31,10 @@ export class CreateCommunityComponent implements OnInit {
   }
 
   createCommunity() {
-    this.communityResponse.name = this.createCommunityForm.get('title')?.value
-    this.communityResponse.description = this.createCommunityForm.get('description')?.value
+    this.communityRequestModel.name = this.createCommunityForm.get('title')?.value
+    this.communityRequestModel.description = this.createCommunityForm.get('description')?.value
 
-    this.communityService.createCommunity(this.communityResponse).subscribe(() => {
+    this.communityService.createCommunity(this.communityRequestModel).subscribe(() => {
       this.router.navigateByUrl('/communities')
     }, () => {
       console.log('Error occurred')
