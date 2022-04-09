0b203580c14c3ea7a7aa6c426a9909ce98c6cc66
Adam Pawelczyk <pawelczyk-adam@outlook.com>
2022-04-09 21:14:08 +0200

    Code refactoring

--- src/app/community/create-community/create-community.component.ts
+++ src/app/community/create-community/create-community.component.ts
@@ -1,9 +1,9 @@
-import { Component, OnInit } from '@angular/core'
-import { FormControl, FormGroup, Validators } from "@angular/forms"
-import { Router } from "@angular/router"
-import { CommunityService } from "../shared/community.service"
-import { NgbActiveModal } from "@ng-bootstrap/ng-bootstrap"
-import {CommunityRequestModel} from "../shared/community-request.model";
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { CommunityService } from '../shared/community.service';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { CommunityRequestModel } from '../shared/community-request.model';
 
 @Component({
   selector: 'app-create-community',
@@ -11,38 +11,37 @@ import {CommunityRequestModel} from "../shared/community-request.model";
   styleUrls: ['./create-community.component.css']
 })
 export class CreateCommunityComponent implements OnInit {
-  createCommunityForm: FormGroup
-  communityRequestModel: CommunityRequestModel
-  title = new FormControl('')
-  description = new FormControl('')
+  createCommunityForm: FormGroup;
+  communityRequestModel: CommunityRequestModel;
+  title = new FormControl('');
+  description = new FormControl('');
 
   constructor(private router: Router, private communityService: CommunityService, public activeModal: NgbActiveModal) {
     this.createCommunityForm = new FormGroup({
       title: new FormControl('', Validators.required),
       description: new FormControl('', Validators.required)
-    })
+    });
+
     this.communityRequestModel = {
       name: '',
       description: ''
-    }
+    };
   }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void { }
 
   createCommunity() {
-    this.communityRequestModel.name = this.createCommunityForm.get('title')?.value
-    this.communityRequestModel.description = this.createCommunityForm.get('description')?.value
+    this.communityRequestModel.name = this.createCommunityForm.get('title')?.value;
+    this.communityRequestModel.description = this.createCommunityForm.get('description')?.value;
 
     this.communityService.createCommunity(this.communityRequestModel).subscribe(() => {
-      this.router.navigateByUrl('/communities')
+      this.router.navigateByUrl('/communities');
     }, () => {
-      console.log('Error occurred')
-    })
+      console.log('Error occurred');
+    });
   }
 
   discard() {
-    this.activeModal.close()
+    this.activeModal.close();
   }
-
 }
