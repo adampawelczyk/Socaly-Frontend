8b156414d42d17b48f74f4be6da63c99b6f4208b
Adam Pawelczyk <pawelczyk-adam@outlook.com>
2022-04-10 23:55:28 +0200

    Code refactoring

--- src/app/community/shared/community.service.ts
+++ src/app/community/shared/community.service.ts
@@ -1,38 +1,36 @@
-import { Injectable } from '@angular/core'
-import { HttpClient } from "@angular/common/http"
-import { Observable } from "rxjs"
-import { CommunityResponseModel } from "./community-response.model"
-import { CommunityRequestModel } from "./community-request.model";
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { CommunityResponseModel } from './community-response.model';
+import { CommunityRequestModel } from './community-request.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CommunityService {
-
   constructor(private httpClient: HttpClient) { }
 
-  getAllCommunities(): Observable<Array<CommunityResponseModel>> {
-    return this.httpClient.get<Array<CommunityResponseModel>>('http://localhost:8090/api/community')
+  getAllCommunities(): Observable<CommunityResponseModel[]> {
+    return this.httpClient.get<CommunityResponseModel[]>('http://localhost:8090/api/community');
   }
 
   createCommunity(communityRequest: CommunityRequestModel): Observable<CommunityResponseModel> {
-    return this.httpClient.post<CommunityResponseModel>('http://localhost:8090/api/community', communityRequest)
+    return this.httpClient.post<CommunityResponseModel>('http://localhost:8090/api/community', communityRequest);
   }
 
   getCommunityDetails(name: string): Observable<CommunityResponseModel> {
-    return this.httpClient.get<CommunityResponseModel>('http://localhost:8090/api/community/' + name)
+    return this.httpClient.get<CommunityResponseModel>('http://localhost:8090/api/community/' + name);
   }
 
-  join(name: string): Observable<any> {
-    return this.httpClient.get('http://localhost:8090/api/community/join/' + name)
+  join(name: string): Observable<Object> {
+    return this.httpClient.get('http://localhost:8090/api/community/join/' + name);
   }
 
-  leave(name: string): Observable<any> {
-    return this.httpClient.get('http://localhost:8090/api/community/leave/' + name)
+  leave(name: string): Observable<Object> {
+    return this.httpClient.get('http://localhost:8090/api/community/leave/' + name);
   }
 
-  getAllCommunitiesForUser(name: string): Observable<Array<CommunityResponseModel>> {
-    return this.httpClient.get<Array<CommunityResponseModel>>(
-      'http://localhost:8090/api/community/getAllCommunitiesForUser/' + name)
+  getAllCommunitiesForUser(name: string): Observable<CommunityResponseModel[]> {
+    return this.httpClient.get<CommunityResponseModel[]>('http://localhost:8090/api/community/getAllCommunitiesForUser/' + name);
   }
 }
