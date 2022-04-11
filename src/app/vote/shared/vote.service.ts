6758722efe64416451fd19692ad471e22d85d9f3
Adam Pawelczyk <pawelczyk-adam@outlook.com>
2022-04-12 00:13:32 +0200

    Code refactoring

--- src/app/vote/shared/vote.service.ts
+++ src/app/vote/shared/vote.service.ts
@@ -1,21 +1,20 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from "@angular/common/http";
-import { PostVoteModel } from "./post-vote.model";
-import { CommentVoteModel } from "./comment-vote.model";
-import { Observable } from "rxjs";
+import { HttpClient } from '@angular/common/http';
+import { PostVoteModel } from './post-vote.model';
+import { CommentVoteModel } from './comment-vote.model';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class VoteService {
-
   constructor(private httpClient: HttpClient) { }
 
-  voteOnPost(voteModel: PostVoteModel): Observable<any> {
+  voteOnPost(voteModel: PostVoteModel): Observable<Object> {
     return this.httpClient.post('http://localhost:8090/api/votes/', voteModel);
   }
 
-  voteOnComment(commentVoteModel: CommentVoteModel): Observable<any> {
-    return this.httpClient.post('http://localhost:8090/api/comments/vote', commentVoteModel)
+  voteOnComment(commentVoteModel: CommentVoteModel): Observable<Object> {
+    return this.httpClient.post('http://localhost:8090/api/comments/vote', commentVoteModel);
   }
 }
