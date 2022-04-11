cf9457ad7287fe08992d647ba6b4b836b53041d2
Adam Pawelczyk <pawelczyk-adam@outlook.com>
2022-04-11 23:45:01 +0200

    Code refactoring

--- src/app/post/create-post/create-post.component.ts
+++ src/app/post/create-post/create-post.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core'
-import { FormControl, FormGroup, Validators } from "@angular/forms"
-import { PostRequestModel } from "../shared/post-request.model"
-import { CommunityResponseModel } from "../../community/shared/community-response.model"
-import { Router } from "@angular/router"
-import { PostService } from "../shared/post.service"
-import { CommunityService } from "../../community/shared/community.service"
-import { throwError } from "rxjs"
-import { NgbActiveModal } from "@ng-bootstrap/ng-bootstrap"
-import { AuthService } from "../../auth/shared/auth.service"
-import { FileService } from "../../shared/file.service"
-import {editorConfig} from "../../../globals";
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { PostRequestModel } from '../shared/post-request.model';
+import { CommunityResponseModel } from '../../community/shared/community-response.model';
+import { Router } from '@angular/router';
+import { PostService } from '../shared/post.service';
+import { CommunityService } from '../../community/shared/community.service';
+import { throwError } from 'rxjs';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { AuthService } from '../../auth/shared/auth.service';
+import { FileService } from '../../shared/file.service';
+import { editorConfig } from '../../../globals';
 
 @Component({
   selector: 'app-create-post',
@@ -17,16 +17,16 @@ import {editorConfig} from "../../../globals";
   styleUrls: ['./create-post.component.css']
 })
 export class CreatePostComponent implements OnInit {
-  createPostForm: FormGroup
-  postPayload: PostRequestModel
-  communities: Array<CommunityResponseModel>
-  files: File[] = []
-  fileUrls: string[] = []
-  active = 1
-  selectedCommunity = "Choose a community"
-  uploadingFiles = false
-  filesUploadProgress = 0
-  postWasPosted = false
+  createPostForm: FormGroup;
+  postRequest: PostRequestModel;
+  communities: CommunityResponseModel[];
+  files: File[] = [];
+  fileUrls: string[] = [];
+  active = 1;
+  selectedCommunity = "Choose a community";
+  uploadingFiles = false;
+  filesUploadProgress = 0;
+  postWasPosted = false;
   editorConfig = editorConfig;
 
   constructor(private router: Router, private postService: PostService, private communityService: CommunityService,
@@ -34,11 +34,11 @@ export class CreatePostComponent implements OnInit {
     this.editorConfig.placeholder = "Text (optional)";
     this.editorConfig.height = 300;
 
-    this.postPayload = {
+    this.postRequest = {
       postName: '',
       description: '',
       communityName: ''
-    }
+    };
   }
 
   ngOnInit(): void {
@@ -46,85 +46,85 @@ export class CreatePostComponent implements OnInit {
       postName: new FormControl('', Validators.required),
       communityName: new FormControl('', Validators.required),
       description: new FormControl('')
-    })
+    });
 
     this.communityService.getAllCommunities().subscribe((data) => {
-      this.communities = data
+      this.communities = data;
     }, error => {
-      throwError(error)
-    })
+      throwError(error);
+    });
   }
 
   ngOnDestroy() {
     if (this.fileUrls.length > 0 && !this.postWasPosted) {
       this.fileUrls.forEach(fileUrl => {
-        this.fileService.removeFile(fileUrl)
-      })
+        this.fileService.removeFile(fileUrl);
+      });
     }
   }
 
   createPost() {
-    this.postPayload.communityName = this.selectedCommunity
-    this.postPayload.postName = this.createPostForm.get('postName')?.value
+    this.postRequest.communityName = this.selectedCommunity;
+    this.postRequest.postName = this.createPostForm.get('postName')?.value;
 
     if (this.active == 1) {
-      this.postPayload.description = this.createPostForm.get('description')?.value
-      this.postPayload.images = []
+      this.postRequest.description = this.createPostForm.get('description')?.value;
+      this.postRequest.images = [];
     } else {
-      this.postPayload.description = ""
-      this.postPayload.images = this.fileUrls
+      this.postRequest.description = "";
+      this.postRequest.images = this.fileUrls;
     }
 
-    this.postService.createPost(this.postPayload).subscribe((id) => {
-      this.postWasPosted = true
-      this.activeModal.close()
-      this.router.navigateByUrl('/view-post/' + id)
+    this.postService.createPost(this.postRequest).subscribe((id) => {
+      this.postWasPosted = true;
+      this.activeModal.close();
+      this.router.navigateByUrl('/view-post/' + id);
     }, error => {
-      throwError(error)
-    })
+      throwError(error);
+    });
   }
 
   discardPost() {
-    this.activeModal.close()
+    this.activeModal.close();
   }
 
   async onSelect(event: { addedFiles: any }) {
-    this.uploadingFiles = true
-    this.files.push(...event.addedFiles)
+    this.uploadingFiles = true;
+    this.files.push(...event.addedFiles);
 
-    await this.uploadFiles(this.files)
-    await new Promise(resolve => setTimeout(resolve, 500))
-    this.uploadingFiles = false
-    this.filesUploadProgress = 0
+    await this.uploadFiles(this.files);
+    await new Promise(resolve => setTimeout(resolve, 500));
+    this.uploadingFiles = false;
+    this.filesUploadProgress = 0;
   }
 
   onRemove(event: File) {
-    let index = this.files.indexOf(event)
-    this.files.splice(index, 1)
-    let deleteFileUrl = this.fileUrls.splice(index, 1)
+    let index = this.files.indexOf(event);
+    this.files.splice(index, 1);
+    let deleteFileUrl = this.fileUrls.splice(index, 1);
 
     if (deleteFileUrl.length > 0) {
-      this.fileService.removeFile(deleteFileUrl[0])
+      this.fileService.removeFile(deleteFileUrl[0]);
     }
   }
 
   private async uploadFiles(files: File[]) {
     for (const file of files) {
-      let fileUrl = await this.fileService.uploadFile(file)
-      this.filesUploadProgress += 100 / files.length
-      this.fileUrls.push(fileUrl)
+      let fileUrl = await this.fileService.uploadFile(file);
+      this.filesUploadProgress += 100 / files.length;
+      this.fileUrls.push(fileUrl);
     }
   }
 
   selectCommunity(name: string) {
-    this.selectedCommunity = name
+    this.selectedCommunity = name;
   }
 
   isCommunityEmpty() {
-    return this.selectedCommunity === 'Choose a community'
+    return this.selectedCommunity === 'Choose a community';
   }
 
   isTitleEmpty() {
-    return this.createPostForm.get('postName')?.value === ''
+    return this.createPostForm.get('postName')?.value === '';
   }
 }
