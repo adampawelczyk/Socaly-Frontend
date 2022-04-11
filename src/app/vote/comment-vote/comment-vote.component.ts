1b3e29d16d75803a953a10724ac036e792cde1fa
Adam Pawelczyk <pawelczyk-adam@outlook.com>
2022-04-12 00:06:28 +0200

    Code refactoring

--- src/app/vote/comment-vote/comment-vote.component.ts
+++ src/app/vote/comment-vote/comment-vote.component.ts
@@ -1,11 +1,11 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { CommentResponseModel } from "../../comment/shared/comment-response.model";
-import { CommentVoteModel } from "../shared/comment-vote.model";
-import { VoteService } from "../shared/vote.service";
-import { AuthService } from "../../auth/shared/auth.service";
-import { CommentService } from "../../comment/shared/comment.service";
-import { VoteType } from "../shared/vote-type";
-import { throwError } from "rxjs";
+import { CommentResponseModel } from '../../comment/shared/comment-response.model';
+import { CommentVoteModel } from '../shared/comment-vote.model';
+import { VoteService } from '../shared/vote.service';
+import { AuthService } from '../../auth/shared/auth.service';
+import { CommentService } from '../../comment/shared/comment.service';
+import { VoteType } from '../shared/vote-type';
+import { throwError } from 'rxjs';
 
 @Component({
   selector: 'app-comment-vote',
@@ -13,42 +13,41 @@ import { throwError } from "rxjs";
   styleUrls: ['./comment-vote.component.css']
 })
 export class CommentVoteComponent implements OnInit {
-  @Input() comment: CommentResponseModel
-  commentVoteModel: CommentVoteModel
+  @Input() comment: CommentResponseModel;
+  commentVoteModel: CommentVoteModel;
 
   constructor(private voteService: VoteService, private authService: AuthService,
               private commentService: CommentService) {
     this.commentVoteModel = {
       voteType: undefined!,
       commentID: undefined!
-    }
+    };
   }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void { }
 
   upvoteComment() {
-    this.commentVoteModel.voteType = VoteType.UPVOTE
-    this.vote()
+    this.commentVoteModel.voteType = VoteType.UPVOTE;
+    this.vote();
   }
 
   downVoteComment() {
-    this.commentVoteModel.voteType = VoteType.DOWNVOTE
-    this.vote()
+    this.commentVoteModel.voteType = VoteType.DOWNVOTE;
+    this.vote();
   }
 
   private vote() {
-    this.commentVoteModel.commentID = this.comment.id
+    this.commentVoteModel.commentID = this.comment.id;
     this.voteService.voteOnComment(this.commentVoteModel).subscribe(() => {
-      this.updateCommentVoteDetails()
+      this.updateCommentVoteDetails();
     }, error => {
-      throwError(error)
-    })
+      throwError(error);
+    });
   }
 
   private updateCommentVoteDetails() {
     this.commentService.getComment(this.comment.id).subscribe(data => {
-      this.comment = data
-    })
+      this.comment = data;
+    });
   }
 }
