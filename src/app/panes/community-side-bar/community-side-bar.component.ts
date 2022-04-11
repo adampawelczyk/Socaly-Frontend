2e4f180f1afaf693d07dec535d688f43c22cddb2
Adam Pawelczyk <pawelczyk-adam@outlook.com>
2022-04-11 23:39:18 +0200

    Code refactoring

--- src/app/panes/community-side-bar/community-side-bar.component.ts
+++ src/app/panes/community-side-bar/community-side-bar.component.ts
@@ -1,8 +1,8 @@
-import { Component, OnInit } from '@angular/core'
-import { CommunityService } from "../../community/shared/community.service"
-import { CommunityResponseModel } from "../../community/shared/community-response.model"
-import { NgbModal } from "@ng-bootstrap/ng-bootstrap"
-import { CreateCommunityComponent } from "../../community/create-community/create-community.component"
+import { Component, OnInit } from '@angular/core';
+import { CommunityService } from '../../community/shared/community.service';
+import { CommunityResponseModel } from '../../community/shared/community-response.model';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { CreateCommunityComponent } from '../../community/create-community/create-community.component';
 
 @Component({
   selector: 'app-community-side-bar',
@@ -10,24 +10,23 @@ import { CreateCommunityComponent } from "../../community/create-community/creat
   styleUrls: ['./community-side-bar.component.css']
 })
 export class CommunitySideBarComponent implements OnInit {
-  communities: Array<CommunityResponseModel>
-  displayViewAll: boolean
+  communities: CommunityResponseModel[];
+  displayViewAll: boolean;
 
   constructor(private communityService: CommunityService, private modal: NgbModal) {
     this.communityService.getAllCommunities().subscribe(data => {
       if (data.length >= 4) {
-        this.communities = data.slice(0, 3)
-        this.displayViewAll = true
+        this.communities = data.slice(0, 3);
+        this.displayViewAll = true;
       } else {
-        this.communities = data
+        this.communities = data;
       }
-    })
+    });
   }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void { }
 
   createCommunity() {
-    this.modal.open(CreateCommunityComponent)
+    this.modal.open(CreateCommunityComponent);
   }
 }
