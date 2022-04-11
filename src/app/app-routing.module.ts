24872a5e61e49fe2a675b2f23c617f78bd2cad76
Adam Pawelczyk <pawelczyk-adam@outlook.com>
2022-04-12 00:14:38 +0200

    Code refactoring

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { SignupComponent } from "./auth/signup/signup.component";
-import { LoginComponent } from "./auth/login/login.component";
-import { HomeComponent } from "./home/home.component";
-import { CreatePostComponent } from "./post/create-post/create-post.component";
-import { CreateCommunityComponent } from "./community/create-community/create-community.component";
-import { CommunitiesComponent } from "./community/communities/communities.component";
-import { ViewPostComponent } from "./post/view-post/view-post.component";
-import { UserProfileComponent } from "./user/user-profile/user-profile.component";
-import { AuthGuard } from "./auth/shared/auth.guard";
-import { CommunityComponent } from "./community/community.component";
+import { SignupComponent } from './auth/signup/signup.component';
+import { LoginComponent } from './auth/login/login.component';
+import { HomeComponent } from './home/home.component';
+import { CreatePostComponent } from './post/create-post/create-post.component';
+import { CreateCommunityComponent } from './community/create-community/create-community.component';
+import { CommunitiesComponent } from './community/communities/communities.component';
+import { ViewPostComponent } from './post/view-post/view-post.component';
+import { UserProfileComponent } from './user/user-profile/user-profile.component';
+import { AuthGuard } from './auth/shared/auth.guard';
+import { CommunityComponent } from './community/community.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
