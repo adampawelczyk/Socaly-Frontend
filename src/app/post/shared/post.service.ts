ffb2410a02728b55325e74e8ecbdbc1b3852aee1
Adam Pawelczyk <pawelczyk-adam@outlook.com>
2022-04-11 23:49:37 +0200

    Code refactoring

--- src/app/post/shared/post.service.ts
+++ src/app/post/shared/post.service.ts
@@ -1,33 +1,32 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from "@angular/common/http";
-import { Observable } from "rxjs";
-import { PostResponseModel } from "./post-response.model";
-import { PostRequestModel } from "./post-request.model";
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { PostResponseModel } from './post-response.model';
+import { PostRequestModel } from './post-request.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PostService {
-
   constructor(private http: HttpClient) { }
 
-  getAllPosts(): Observable<Array<PostResponseModel>> {
-    return this.http.get<Array<PostResponseModel>>('http://localhost:8090/api/posts')
+  getAllPosts(): Observable<PostResponseModel[]> {
+    return this.http.get<PostResponseModel[]>('http://localhost:8090/api/posts');
   }
 
-  createPost(postPayload: PostRequestModel): Observable<Number> {
-    return this.http.post<Number>('http://localhost:8090/api/posts', postPayload)
+  createPost(postRequestModel: PostRequestModel): Observable<number> {
+    return this.http.post<number>('http://localhost:8090/api/posts', postRequestModel);
   }
 
   getPost(id: number): Observable<PostResponseModel> {
-    return this.http.get<PostResponseModel>('http://localhost:8090/api/posts/' + id)
+    return this.http.get<PostResponseModel>('http://localhost:8090/api/posts/' + id);
   }
 
-  getAllPostsByUser(name: String): Observable<PostResponseModel[]> {
-    return this.http.get<PostResponseModel[]>('http://localhost:8090/api/posts/by-user/' + name)
+  getAllPostsByUser(username: string): Observable<PostResponseModel[]> {
+    return this.http.get<PostResponseModel[]>('http://localhost:8090/api/posts/by-user/' + username);
   }
 
-  getAllPostsByCommunity(name: String): Observable<PostResponseModel[]> {
-    return this.http.get<PostResponseModel[]>('http://localhost:8090/api/posts/by-community/' + name)
+  getAllPostsByCommunity(communityName: string): Observable<PostResponseModel[]> {
+    return this.http.get<PostResponseModel[]>('http://localhost:8090/api/posts/by-community/' + communityName);
   }
 }
