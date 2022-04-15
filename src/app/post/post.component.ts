6266406d7aa5357a55d6eaa02a5f347c822d2aec
Adam Pawelczyk <pawelczyk-adam@outlook.com>
2022-04-15 23:53:08 +0200

    Code refactoring

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,14 +1,14 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { PostService } from "./shared/post.service";
-import { PostResponseModel } from "./shared/post-response.model";
-import { faCommentAlt } from "@fortawesome/free-solid-svg-icons";
-import { ActivatedRoute, Router } from "@angular/router";
-import { throwError } from "rxjs";
-import { CommentService } from "../comment/shared/comment.service";
-import { CommentRequestModel } from "../comment/shared/comment-request.model";
-import { FormControl, FormGroup, Validators } from "@angular/forms";
-import { CommentResponseModel } from "../comment/shared/comment-response.model";
-import { editorConfig } from "../../globals";
+import { PostService } from './shared/post.service';
+import { PostResponseModel } from './shared/post-response.model';
+import { faCommentAlt } from '@fortawesome/free-solid-svg-icons';
+import { ActivatedRoute, Router } from '@angular/router';
+import { throwError } from 'rxjs';
+import { CommentService } from '../comment/shared/comment.service';
+import { CommentRequestModel } from '../comment/shared/comment-request.model';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { CommentResponseModel } from '../comment/shared/comment-response.model';
+import { editorConfig } from '../../globals';
 
 @Component({
   selector: 'app-post',
@@ -30,7 +30,7 @@ export class PostComponent implements OnInit {
   constructor(private postService: PostService, private router: Router, private commentService: CommentService,
               private activateRoute: ActivatedRoute) {
     this.postId = this.activateRoute.snapshot.params.id;
-    this.editorConfig.placeholder = "What are your thoughts?"
+    this.editorConfig.placeholder = "What are your thoughts?";
     this.editorConfig.height = 174;
 
     this.commentForm = new FormGroup({
@@ -50,18 +50,18 @@ export class PostComponent implements OnInit {
   }
 
   postDescriptionIsNotEmpty(post: PostResponseModel) {
-    return post.description != ""
+    return post.description != "";
   }
 
   postImagesAreNotEmpty(post: PostResponseModel) {
-    return post.images && post.images?.length > 0
+    return post.images && post.images?.length > 0;
   }
 
   postComment() {
     this.commentPayload.text = this.commentForm.get('text')?.value;
     this.commentForm.get('text')?.setValue('');
 
-    this.commentService.postComment(this.commentPayload).subscribe(data => {
+    this.commentService.postComment(this.commentPayload).subscribe(() => {
       this.getCommentsForPost();
     }, error => {
       throwError(error);
@@ -82,9 +82,9 @@ export class PostComponent implements OnInit {
 
   showNavigationArrows(post: PostResponseModel) {
     if (post.images !== undefined) {
-      return post.images.length > 1
+      return post.images.length > 1;
     } else {
-      return false
+      return false;
     }
   }
 }
