1f9848e63c71330f3bc6050c968d9882317519cf
Adam Pawelczyk <pawelczyk-adam@outlook.com>
2022-04-30 21:21:31 +0200

    Create signup module

---
+++ src/app/auth/signup/signup.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SignupComponent } from './signup.component';
+import { ReactiveFormsModule } from '@angular/forms';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    ReactiveFormsModule
+  ],
+  declarations: [
+    SignupComponent
+  ],
+  exports: [
+    SignupComponent
+  ]
+})
+
+export class SignupModule { }
