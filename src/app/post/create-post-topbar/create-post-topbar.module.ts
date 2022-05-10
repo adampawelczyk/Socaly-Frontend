39d990a83b1d381e653a566ca0bdc770eae6ac83
Adam Pawelczyk <pawelczyk-adam@outlook.com>
2022-05-10 23:25:04 +0200

    Create create-post-topbar module

---
+++ src/app/post/create-post-topbar/create-post-topbar.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CreatePostTopbarComponent } from './create-post-topbar.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [
+    CreatePostTopbarComponent
+  ],
+  exports: [
+    CreatePostTopbarComponent
+  ]
+})
+
+export class CreatePostTopbarModule { }
