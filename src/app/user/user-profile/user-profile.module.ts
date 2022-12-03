6d0469947302dccf088a25444470c51fb4d66c5b
Adam Pawelczyk <pawelczyk-adam@outlook.com>
2022-12-03 22:22:52 +0100

    Add navigation on user profile page

--- src/app/user/user-profile/user-profile.module.ts
+++ src/app/user/user-profile/user-profile.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 import { UserProfileComponent } from './user-profile.component';
 import { PostModule } from '../../post/post.module';
 import { AppRoutingModule } from '../../app-routing.module';
+import { NgbNavModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    PostModule,
-    AppRoutingModule
-  ],
+    imports: [
+        CommonModule,
+        PostModule,
+        AppRoutingModule,
+        NgbNavModule
+    ],
   declarations: [
     UserProfileComponent
   ],
