f795a0f5ddeda946a45160c3a05a3577de62032c
Adam Pawelczyk <pawelczyk-adam@outlook.com>
2023-02-22 22:45:41 +0100

    Add getUserDetails method

--- src/app/user/shared/user.service.ts
+++ src/app/user/shared/user.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { UserModel } from './user.model';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
 
+  getUserDetails(username: string): Observable<UserModel> {
+    return this.httpClient.get<UserModel>('http://localhost:8090/api/user/getUserDetails/' + username)
+  }
 }
