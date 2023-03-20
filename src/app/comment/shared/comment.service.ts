fbf6292c64d3b51d95ea1a2c1243951d99e17be9
Adam Pawelczyk <pawelczyk-adam@outlook.com>
2023-03-20 21:49:02 +0100

    API fixes after backend refactoring

--- src/app/comment/shared/comment.service.ts
+++ src/app/comment/shared/comment.service.ts
@@ -11,26 +11,26 @@ export class CommentService {
   constructor(private httpClient: HttpClient) { }
 
   getComment(commentId: number): Observable<CommentResponseModel> {
-    return this.httpClient.get<CommentResponseModel>('http://localhost:8090/api/comments/' + commentId);
+    return this.httpClient.get<CommentResponseModel>('http://localhost:8090/api/comment/get/' + commentId);
   }
 
   getAllCommentsForPost(postId: number): Observable<CommentResponseModel[]> {
-    return this.httpClient.get<CommentResponseModel[]>('http://localhost:8090/api/comments/by-post/' + postId);
+    return this.httpClient.get<CommentResponseModel[]>('http://localhost:8090/api/comment/get/all/by-post/' + postId);
   }
 
   getSubCommentsForComment(commentId: number): Observable<CommentResponseModel[]> {
-    return this.httpClient.get<CommentResponseModel[]>('http://localhost:8090/api/comments/subcomments/' + commentId);
+    return this.httpClient.get<CommentResponseModel[]>('http://localhost:8090/api/comment/get/all/sub-comments/' + commentId);
   }
 
   postComment(commentPayload: CommentRequestModel): Observable<Object> {
-    return this.httpClient.post('http://localhost:8090/api/comments', commentPayload);
+    return this.httpClient.post('http://localhost:8090/api/comment/create', commentPayload);
   }
 
   editComment(commentId: number, text: string): Observable<Object> {
-    return this.httpClient.post('http://localhost:8090/api/comments/edit/' + commentId, text);
+    return this.httpClient.post('http://localhost:8090/api/comment/edit/' + commentId, text);
   }
 
   getAllCommentsByUser(name: string): Observable<CommentResponseModel[]> {
-    return this.httpClient.get<CommentResponseModel[]>('http://localhost:8090/api/comments/by-user/' + name);
+    return this.httpClient.get<CommentResponseModel[]>('http://localhost:8090/api/comment/get/all/by-user/' + name);
   }
 }
