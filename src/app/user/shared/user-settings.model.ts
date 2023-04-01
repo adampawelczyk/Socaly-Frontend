65f2ce5e2b758e144a19f4feaea3333aff974df8
Adam Pawelczyk <pawelczyk-adam@outlook.com>
2023-04-01 23:08:58 +0200

    Create UserSettingsModel interface

---
+++ src/app/user/shared/user-settings.model.ts
@@ -0,0 +1,8 @@
+export interface UserSettingsModel {
+  createdDate: string;
+  profileImage: string;
+  profileBanner: string;
+  description: string;
+  email: string;
+  emailVerified: boolean;
+}
