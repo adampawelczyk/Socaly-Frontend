04e97746f1dba06651bf18458a7e40e3832deba2
Adam Pawelczyk <pawelczyk-adam@outlook.com>
2023-04-03 22:13:22 +0200

    Get username, user settings and create and initialize changeDescriptionForm

--- src/app/user/settings/settings.component.ts
+++ src/app/user/settings/settings.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../shared/user.service';
+import { AuthService } from '../../auth/shared/auth.service';
+import { UserSettingsModel } from '../shared/user-settings.model';
+import { UntypedFormControl, UntypedFormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-settings',
@@ -6,10 +10,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./settings.component.scss']
 })
 export class SettingsComponent implements OnInit {
+  name: string;
+  userSettings: UserSettingsModel;
+  changeDescriptionForm: UntypedFormGroup;
 
-  constructor() { }
+  constructor(private authService: AuthService ,private userService: UserService) { }
 
   ngOnInit(): void {
-  }
+    this.authService.getUsername();
+    this.userService.getUserSettings().subscribe(data => {
+      this.userSettings = data;
+    })
 
+    this.changeDescriptionForm = new UntypedFormGroup({
+      description: new UntypedFormControl('', Validators.required)
+    });
+
+  }
 }
