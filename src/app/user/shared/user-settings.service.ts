ca0651ae026c4571fe3c0f32924df439161f1abe
Adam Pawelczyk <pawelczyk-adam@outlook.com>
2023-04-09 22:59:41 +0200

    Create getUserSettings method

--- src/app/user/shared/user-settings.service.ts
+++ src/app/user/shared/user-settings.service.ts
@@ -1,8 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { UserSettingsModel } from './user-settings.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserSettingsService {
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+
+  getUserSettings(): Observable<UserSettingsModel> {
+    return this.httpClient.get<UserSettingsModel>('http://localhost:8090/api/user/settings/get')
+  }
+
 }
