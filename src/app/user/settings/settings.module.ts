3aa8b8c4f497d8e750976dc3f98c6d4b3a30805d
Adam Pawelczyk <pawelczyk-adam@outlook.com>
2023-04-09 23:06:58 +0200

    Add ReactiveFormsModule and NgbModule to the imports

--- src/app/user/settings/settings.module.ts
+++ src/app/user/settings/settings.module.ts
@@ -2,15 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SettingsComponent } from './settings.component';
 import { AppRoutingModule } from '../../app-routing.module';
-import { NgbNavModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
     imports: [
         CommonModule,
         AppRoutingModule,
-        NgbNavModule,
-        ReactiveFormsModule
+        ReactiveFormsModule,
+        NgbModule
     ],
   declarations: [
     SettingsComponent
