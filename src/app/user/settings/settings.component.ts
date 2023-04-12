a77658fdf85ba8b02ab53e42ad084fe6ca39311d
Adam Pawelczyk <pawelczyk-adam@outlook.com>
2023-04-12 23:42:49 +0200

    Create methods to compare communityContentSort from the userSettings to the Sorting enum

--- src/app/user/settings/settings.component.ts
+++ src/app/user/settings/settings.component.ts
@@ -79,4 +79,36 @@ export class SettingsComponent implements OnInit {
   onChangeTab(url: NgbNavChangeEvent){
     this.location.replaceState("settings/" + url.nextId);
   }
+
+  isHot() {
+    return this.userSettings.communityContentSort.valueOf().toString() === Sorting[Sorting.HOT] || this.userSettings.communityContentSort === Sorting.HOT;
+  }
+
+  isNew() {
+    return this.userSettings.communityContentSort.valueOf().toString() === Sorting[Sorting.NEW] || this.userSettings.communityContentSort === Sorting.NEW;
+  }
+
+  isTop() {
+    return !this.isHot() && !this.isNew();
+  }
+
+  isTopToday() {
+    return this.userSettings.communityContentSort.valueOf().toString() === Sorting[Sorting.TOP_TODAY] || this.userSettings.communityContentSort === Sorting.TOP_TODAY;
+  }
+
+  isTopWeek() {
+    return this.userSettings.communityContentSort.valueOf().toString() === Sorting[Sorting.TOP_WEEK] || this.userSettings.communityContentSort === Sorting.TOP_WEEK;
+  }
+
+  isTopMonth() {
+    return this.userSettings.communityContentSort.valueOf().toString() === Sorting[Sorting.TOP_MONTH] || this.userSettings.communityContentSort === Sorting.TOP_MONTH;
+  }
+
+  isTopYear() {
+    return this.userSettings.communityContentSort.valueOf().toString() === Sorting[Sorting.TOP_YEAR] || this.userSettings.communityContentSort === Sorting.TOP_YEAR;
+  }
+
+  isTopAllTime() {
+    return this.userSettings.communityContentSort.valueOf().toString() === Sorting[Sorting.TOP_ALL_TIME] || this.userSettings.communityContentSort === Sorting.TOP_ALL_TIME;
+  }
 }
