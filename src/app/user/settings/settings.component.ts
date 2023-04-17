935a97fe4f10f39f5949e48c67075759d988f45b
Adam Pawelczyk <pawelczyk-adam@outlook.com>
2023-04-17 23:35:05 +0200

    Create a method to change profile image

--- src/app/user/settings/settings.component.ts
+++ src/app/user/settings/settings.component.ts
@@ -9,6 +9,7 @@ import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 import { NgbNavChangeEvent } from '@ng-bootstrap/ng-bootstrap';
 import { Sorting } from '../shared/sorting';
+import { FileService } from '../../shared/file.service';
 
 @Component({
   selector: 'app-settings',
@@ -24,9 +25,11 @@ export class SettingsComponent implements OnInit {
   changeDescriptionForm: UntypedFormGroup;
   availableCharacters: number;
   SortingType = Sorting;
+  file: File;
+  isProfileImageUploading: boolean = false;
 
   constructor(private authService: AuthService ,private userService: UserService, private userSettingsService: UserSettingsService,
-              private route: ActivatedRoute, private location: Location) { }
+              private route: ActivatedRoute, private location: Location, private fileService: FileService) { }
 
   ngOnInit(): void {
     this.route.data.subscribe(data => {
@@ -116,5 +119,27 @@ export class SettingsComponent implements OnInit {
 
   isTopAllTime() {
     return this.userSettings.communityContentSort.valueOf().toString() === Sorting[Sorting.TOP_ALL_TIME] || this.userSettings.communityContentSort === Sorting.TOP_ALL_TIME;
+  }
+
+  async changeProfileImage(event: Event): Promise<void> {
+    const target = event.target as HTMLInputElement;
+    const file: File = (target.files as FileList)[0];
+
+    if (file) {
+      this.isProfileImageUploading = true;
+      let fileUrl = await this.fileService.uploadFile(file);
+      this.isProfileImageUploading = false;
+
+      this.userService.changeProfileImage(fileUrl).subscribe(() => {
+        this.userService.getUserDetails(this.authService.getUsername()).subscribe(data => {
+          if (this.user.profileImage.includes('uploads')) {
+            this.fileService.removeFile(this.user.profileImage);
+          }
+
+          this.user.profileImage = data.profileImage;
+          this.userService.reloadUserDetails();
+        })
+      })
+    }
   }
 }
