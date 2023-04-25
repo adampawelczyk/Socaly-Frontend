f064d66cb8b09a1ae340749d95adddab73f9ae28
Adam Pawelczyk <pawelczyk-adam@outlook.com>
2023-04-25 23:35:01 +0200

    Remove the style attribute when email update modal is opened

--- src/app/user-settings/email-update/email-update.component.ts
+++ src/app/user-settings/email-update/email-update.component.ts
@@ -10,6 +10,8 @@ export class EmailUpdateComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    const body = document.getElementsByTagName('body')[0];
+    body.removeAttribute('style');
   }
 
 }
