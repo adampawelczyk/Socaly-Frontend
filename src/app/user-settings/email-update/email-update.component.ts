8d7df9a90cd045de1da35f7ddcdff70b9cec1523
Adam Pawelczyk <pawelczyk-adam@outlook.com>
2023-04-25 23:38:42 +0200

    Create the emailUpdateForm variable and initialize it

--- src/app/user-settings/email-update/email-update.component.ts
+++ src/app/user-settings/email-update/email-update.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { UntypedFormControl, UntypedFormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-email-update',
@@ -7,12 +8,18 @@ import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
   styleUrls: ['./email-update.component.scss']
 })
 export class EmailUpdateComponent implements OnInit {
+  emailUpdateForm: UntypedFormGroup;
 
   constructor(private activeModal: NgbActiveModal) { }
 
   ngOnInit(): void {
     const body = document.getElementsByTagName('body')[0];
     body.removeAttribute('style');
+
+    this.emailUpdateForm = new UntypedFormGroup({
+      password: new UntypedFormControl('', Validators.required),
+      email: new UntypedFormControl('', Validators.required)
+    });
   }
 
   discard() {
