d2355efff9fe87a78c97b47be8088de8ed875d06
Adam Pawelczyk <pawelczyk-adam@outlook.com>
2023-04-26 23:07:26 +0200

    Create an updateEmail method to open a modal to update the user's email

--- src/app/user-settings/account-settings/account-settings.component.ts
+++ src/app/user-settings/account-settings/account-settings.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../../user/shared/user.service';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { EmailUpdateComponent } from '../email-update/email-update.component';
 
 @Component({
   selector: 'app-account-settings',
@@ -10,7 +12,7 @@ export class AccountSettingsComponent implements OnInit {
   email: string;
   isEmailVerified: boolean;
 
-  constructor(private userService: UserService) { }
+  constructor(private userService: UserService, private modal: NgbModal) { }
 
   ngOnInit(): void {
     this.userService.getEmail().subscribe(data => {
@@ -20,5 +22,14 @@ export class AccountSettingsComponent implements OnInit {
     this.userService.isEmailVerified().subscribe(data => {
       this.isEmailVerified = data;
     });
+  }
+
+  updateEmail() {
+    let emailUpdateModal = this.modal.open(EmailUpdateComponent, {size: "md"});
+    emailUpdateModal.closed.subscribe(() => {
+      this.userService.getEmail().subscribe(data => {
+        this.email = data;
+      });
+    })
   }
 }
