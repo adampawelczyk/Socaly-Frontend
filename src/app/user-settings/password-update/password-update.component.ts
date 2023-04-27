8e4a7e2644294f54c2b2b834489b964c9d7c1f92
Adam Pawelczyk <pawelczyk-adam@outlook.com>
2023-04-27 23:38:39 +0200

    Create the passwordUpdateForm variable and initialize it

--- src/app/user-settings/password-update/password-update.component.ts
+++ src/app/user-settings/password-update/password-update.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UntypedFormControl, UntypedFormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-password-update',
@@ -6,12 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./password-update.component.scss']
 })
 export class PasswordUpdateComponent implements OnInit {
+  passwordUpdateForm: UntypedFormGroup;
 
   constructor() { }
 
   ngOnInit(): void {
     const body = document.getElementsByTagName('body')[0];
     body.removeAttribute('style');
+
+    this.passwordUpdateForm = new UntypedFormGroup({
+      currentPassword: new UntypedFormControl('', Validators.required),
+      newPassword: new UntypedFormControl('', Validators.required),
+      confirmNewPassword: new UntypedFormControl('', Validators.required)
+    });
   }
 
 }
