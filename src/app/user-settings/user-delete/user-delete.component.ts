1cdcd45c070f33f3acfbfe83b25508bf028f14ac
Adam Pawelczyk <pawelczyk-adam@outlook.com>
2023-05-01 22:56:30 +0200

    Create the delete method

--- src/app/user-settings/user-delete/user-delete.component.ts
+++ src/app/user-settings/user-delete/user-delete.component.ts
@@ -2,6 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { UntypedFormControl, UntypedFormGroup, Validators } from '@angular/forms';
 import { UserDeleteRequestModel } from '../shared/user-delete-request.model';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { UserService } from '../../user/shared/user.service';
+import { AuthService } from '../../auth/shared/auth.service';
+import { throwError } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-user-delete',
@@ -12,7 +16,8 @@ export class UserDeleteComponent implements OnInit {
   userDeleteForm: UntypedFormGroup;
   userDeletePayload: UserDeleteRequestModel;
 
-  constructor(private activeModal: NgbActiveModal) {
+  constructor(private activeModal: NgbActiveModal, private userService: UserService, private authService: AuthService,
+              private router: Router) {
   }
 
   ngOnInit(): void {
@@ -32,5 +37,18 @@ export class UserDeleteComponent implements OnInit {
 
   discard() {
     this.activeModal.close();
+  }
+
+  delete() {
+    this.userDeletePayload.username = this.userDeleteForm.get('username')?.value;
+    this.userDeletePayload.password = this.userDeleteForm.get('password')?.value;
+
+    this.userService.delete(this.userDeletePayload).subscribe(() => {
+      this.activeModal.close();
+      this.authService.logout();
+      this.router.navigateByUrl('');
+    }, error => {
+      throwError(error);
+    })
   }
 }
