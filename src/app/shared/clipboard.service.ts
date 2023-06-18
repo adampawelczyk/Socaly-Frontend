7c77e2a96c3321cebb908e088ac5e3925f645c58
Adam Pawelczyk <pawelczyk-adam@outlook.com>
2023-06-18 23:46:22 +0200

    Create the clipboard service

---
+++ src/app/shared/clipboard.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ClipboardService {
+  constructor() { }
+
+  public writeText(textToCopy: string): Promise<void> {
+    return new Promise(
+      (resolve, reject): void => {
+        let success = false;
+        const listener = (clipboardEvent: ClipboardEvent): void => {
+          clipboardEvent.clipboardData?.setData('text/plain', textToCopy);
+          clipboardEvent.preventDefault();
+          success = true;
+        };
+
+        const input = document.createElement('input');
+        document.body.appendChild(input);
+        input.select();
+
+        document.addEventListener('copy', listener);
+        document.execCommand('copy');
+        document.removeEventListener('copy', listener);
+        document.body.removeChild(input);
+        success ? resolve() : reject();
+      },
+    );
+  }
+}
