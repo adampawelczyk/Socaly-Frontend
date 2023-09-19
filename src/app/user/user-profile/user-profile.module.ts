e6834f561d4c83255ef339e00e1194873d6a4d69
Adam Pawelczyk <pawelczyk-adam@outlook.com>
2023-09-19 23:14:12 +0200

    Import the CommentModule

--- src/app/user/user-profile/user-profile.module.ts
+++ src/app/user/user-profile/user-profile.module.ts
@@ -5,15 +5,17 @@ import { PostModule } from '../../post/post/post.module';
 import { AppRoutingModule } from '../../app-routing.module';
 import { NgbNavModule } from '@ng-bootstrap/ng-bootstrap';
 import { UserSidebarModule } from '../user-sidebar/user-sidebar.module';
+import { CommentModule } from '../../comment/comment/comment.module';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    PostModule,
-    AppRoutingModule,
-    NgbNavModule,
-    UserSidebarModule
-  ],
+    imports: [
+        CommonModule,
+        PostModule,
+        AppRoutingModule,
+        NgbNavModule,
+        UserSidebarModule,
+        CommentModule
+    ],
   declarations: [
     UserProfileComponent
   ],
