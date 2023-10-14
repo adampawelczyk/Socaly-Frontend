2f74ac949956fe71cea1c9577e219441b0a11f6c
Adam Pawelczyk <pawelczyk-adam@outlook.com>
2023-10-14 20:54:05 +0200

    Renamed the 'signup' method to 'signUp'

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -33,7 +33,7 @@ export class HeaderComponent implements OnInit {
     this.username = this.localStorage.retrieve('username');
   }
 
-  signup() {
+  signUp() {
     this.modal.open(SignUpComponent);
   }
 
